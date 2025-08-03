a6f401682adcf738c5f87c12a72756cd1b2f0582
Xitong Gao <238821+admk@users.noreply.github.com>
2025-08-03 17:50:22 +0800

    fix(processors): broken init

--- sembr/processors/__init__.py
+++ sembr/processors/__init__.py
@@ -5,10 +5,11 @@ Grammar-based text processors for different file types.
 from .base import BaseProcessor
 from .latex import LaTeXProcessor
 from .markdown import MarkdownProcessor
-from .plaintext import PlaintextProcessor
+from .plaintext import PlainTextProcessor
 from .utils import get_processor, detect_file_type_from_text
 
 
 __all__ = [
-    'BaseProcessor', 'LaTeXProcessor', 'MarkdownProcessor', 'PlaintextProcessor',
+    'BaseProcessor',
+    'LaTeXProcessor', 'MarkdownProcessor', 'PlainTextProcessor',
     'get_processor', 'detect_file_type_from_text']
